048e8420a5de2d2e59b0ca2e8756e75cff0ffff9
CYN <moxuec@gmail.com>
2018-04-20 01:09:13 +0800

    律师事务所出错，标题提取有空行

--- 百度requests.py
+++ 百度requests.py
@@ -12,6 +12,7 @@ class BaiDu:
         self.port = 3306
         self.db = 'spider'
         self.keyword = keyword
+        self.table = 'baidu'
 
         self.url = 'http://www.baidu.com/s?wd=' + self.keyword + '&ie=UTF-8'
 
@@ -39,31 +40,44 @@ class BaiDu:
         sel = etree.HTML(html)
         urls = sel.xpath('//div[@id>3000]/div[1]/h3/a/@href')  # 广告链接
         land_urls = sel.xpath('//div[@id>3000]/div[1]/h3/a/@data-landurl')  # 落地页链接
-        display_urls = sel.xpath('//div[@id>3000]/div[2]/div/div[2]/div[2]/a[1]/span[1]/text()')
-
-        a = sel.xpath('//div[@id>3000]/div[1]/h3/a')
+        times = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         titles = []
+        a = sel.xpath('//div[@id>3000]/div[1]/h3/a')
         for i in a :
             title = i.xpath('string(.)')
             titles.append(title)
-        ids = range(len(titles))
-
-        for id,title,url,land_url in zip(ids,titles,urls,land_urls):
-            data = {
-                'id':id,
-                'keyword':self.keyword,
-                'title':title,
-                'url':url,
-                'land_url':land_url
-            }
-            self.save('baidu',data)
-
+        rankings = range(1,len(titles)+1)
+        display_urls = []
+        for land_url in land_urls:
+            if 'https' in land_url:
+                display_url = land_url.split('https://')[1].split('/')[0]
+            else:
+                display_url = land_url.split('http://')[1].split('/')[0]
+            display_urls.append(display_url)
+        if len(urls)==len(display_urls):
+            for ranking, title, url, land_url, display_url in zip(rankings, titles, urls, land_urls, display_urls):
+                data = {
+                    'time': times,
+                    'ranking': ranking,
+                    'keyword': self.keyword,
+                    'title': title,
+                    'url': url,
+                    'land_url': land_url,
+                    'display_url': display_url
+                }
+                self.save(self.table, data)
+        else:
+            print('titles:',titles)
+            print('urls:', urls)
+            print('land_urls:', land_urls)
+            print('display_urls:', display_urls)
+            print('rankings:', rankings)
+            print('数据未对齐')
 
     def main(self):
         html = self.get_html()
         self.parse(html)
-        print('OK')
 
 if __name__=="__main__":
-    app = BaiDu("法律咨询")
+    app = BaiDu("律师事务所")
     app.main()
